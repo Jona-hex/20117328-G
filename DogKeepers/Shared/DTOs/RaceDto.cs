8a724f980c0b023ec455d41e19fd499a55b6e639
Jonathan Barocio <jbarocio2@ucol.mx>
2021-01-21 19:06:49 -0600

    commit

--- DogKeepers/Shared/DTOs/RaceDto.cs
+++
@@ -1,8 +0,0 @@
-namespace DogKeepers.Shared.DTOs
-{
-    public class RaceDto
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-    }
-}
--- DogKeepers/Shared/DTOs/SizeDto.cs
+++
@@ -1,8 +0,0 @@
-namespace DogKeepers.Shared.DTOs
-{
-    public class SizeDto
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-    }
-}
